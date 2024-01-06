d1b70cd4fecec06ba5513fca52435e11f9de2260
nitinraj2001 <nitin2019cs094abesit@gmail.com>
2024-01-06 19:45:32 +0530

    Added logic for Synchronous communication.

--- inventory-service/src/main/java/com/nitin/inventoryservice/Service/InventoryService.java
+++ inventory-service/src/main/java/com/nitin/inventoryservice/Service/InventoryService.java
@@ -1,9 +1,13 @@
 package com.nitin.inventoryservice.Service;
 
+import com.nitin.inventoryservice.dto.InventoryResponse;
 import com.nitin.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -13,8 +17,12 @@ public class InventoryService {
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly=true)
-    public boolean isInStock(String skuCode){
-        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    public List<InventoryResponse> isInStock(List<String> skuCode){
+         return inventoryRepository.findBySkuCodeIn(skuCode).stream()
+                .map(inventory -> InventoryResponse.builder()
+                        .skuCode(inventory.getSkuCode())
+                        .isInStock(inventory.getQuantity()>0?true:false).build()
+                ).collect(Collectors.toList());
     }
 
 }
--- inventory-service/src/main/java/com/nitin/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/nitin/inventoryservice/controller/InventoryController.java
@@ -1,9 +1,12 @@
 package com.nitin.inventoryservice.controller;
 
 import com.nitin.inventoryservice.Service.InventoryService;
+import com.nitin.inventoryservice.dto.InventoryResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/inventory")
@@ -12,9 +15,9 @@ public class InventoryController {
 
     private final InventoryService inventoryService;
 
-    @GetMapping("{sku-code}")
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public boolean isInStock(@PathVariable("sku-code") String skuCode){
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode){
        return inventoryService.isInStock(skuCode);
     }
 }
